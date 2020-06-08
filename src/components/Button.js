636f7c5374d763abf80135a71841b7040680b0fa
devsangho <gaonmail@cau.ac.kr>
2020-06-08 23:34:13 +0900

    outline 추가

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 import classNames from 'classnames'
 import './Button.scss'
 
-function Button({ children, size, color }) {
-    return <button className={classNames('Button', size, color)}>{children}</button>
-}
+function Button({ children, size, color, outline }) {
+    return <button className={classNames('Button', size, color, {outline})}>{children}</button>
+} // {```} ``` 안에 있는 값이 true일 때만 CSS 클래스를 적용시키겠다는 뜻.
 
 Button.defaultProps = {
     size: 'medium',
